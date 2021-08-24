d24602d3463ee1e8d296acd9a788882f9e503ee8
Sujal Kokh Shrestha <sujal.kokh@gmail.com>
2021-08-24 13:08:47 +0545

    chore: config file added

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+import 'package:learning_flutter/enums/env.dart';
+
+class Config {
+  static const AppEnv ENV = AppEnv.development;
+  static const String BASE_URL = 'https://animechan.vercel.app/api';
+}
