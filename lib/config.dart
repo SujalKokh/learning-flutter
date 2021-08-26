2a9a9f3b85b3e45aec9902c8e7cfeac87ac5d649
Sujal Kokh Shrestha <sujal.kokh@gmail.com>
2021-08-26 10:40:44 +0545

    chore: added weather url in config

--- lib/config.dart
+++ lib/config.dart
@@ -3,4 +3,5 @@ import 'package:learning_flutter/enums/env.dart';
 class Config {
   static const AppEnv ENV = AppEnv.development;
   static const String BASE_URL = 'https://animechan.vercel.app/api';
+  static const String WEATHER_URL = 'https://www.metaweather.com/api';
 }
